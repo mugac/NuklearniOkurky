395042cd8dd868e332f5be01b33f710d564433ef
strangertwo <h.volhejn@gmail.com>
2024-11-15 14:23:52 +0100

    vite-init

---
+++ src/main/vite/src/app.tsx
@@ -0,0 +1,9 @@
+import { FunctionalComponent } from "preact";
+
+export const App: FunctionalComponent = () => {
+    return (
+        <div class="flex flex-col grow m-2 md:m-4">
+            <h1>Example Vite App</h1>
+        </div>
+    );
+};
---
+++ src/main/vite/src/index.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+import { hydrate } from "preact";
+import render from "preact-render-to-string/jsx";
+
+import { App } from "./app";
+
+if (typeof window !== "undefined") {
+    hydrate(<App />, document.querySelector("#app")!);
+}
+
+export async function prerender() {
+    const html = render(<App />, {}, { pretty: true });
+
+    return {
+        html,
+    };
+}
