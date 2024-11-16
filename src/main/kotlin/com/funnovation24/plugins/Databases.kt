902798fda143aba364dd3a095c09d57517c39cbc
strangertwo <h.volhejn@gmail.com>
2024-11-16 05:32:50 +0100

    api routes

--- src/main/kotlin/com/funnovation24/plugins/Databases.kt
+++ src/main/kotlin/com/funnovation24/plugins/Databases.kt
@@ -19,10 +19,10 @@ fun Application.configureDatabases() {
 fun Application.connectToMariaDb(): Database {
     val user = System.getenv("DB_USER") ?: "nuklearniuser"
     val password = System.getenv("DB_PASSWORD") ?: "P7x5kMScw"
-    val host = System.getenv("DB_HOST") ?: "10.128.40.90"
-    val port = System.getenv("DB_PORT") ?: "3306"
-//    val host = System.getenv("DB_HOST") ?: "10.128.40.94"
-//    val port = System.getenv("DB_PORT") ?: "55712"
+//    val host = System.getenv("DB_HOST") ?: "10.128.40.90"
+//    val port = System.getenv("DB_PORT") ?: "3306"
+    val host = System.getenv("DB_HOST") ?: "10.128.40.94"
+    val port = System.getenv("DB_PORT") ?: "55712"
     val dbName = System.getenv("DB_NAME") ?: "nuklearni_okurky"
 
     //  jdbc:mariadb://HOST:PORT/DBNAME
